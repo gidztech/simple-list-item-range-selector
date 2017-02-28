2c3008a76dbba55072c40ea24283763ab67ad39a
gideon.pyzer <gideon.pyzer@huddle.com>
2017-02-28 11:44:52 +0000

    Make it more componenty

--- list-selection.js
+++ list-selection.js
@@ -1,57 +1,27 @@
 (function () {
-
-    let listItems = [];
-
-    function init() {
-        // setup data
-        setupData();
-        // create view
-        new SelectionView($('.selection-view'));
-    }
-
-    function SelectionView(element) {
-        let el = element;
-        let render = function() {
-            let templateTag = $("#selectionTemplate").html();
-            Mustache.parse(templateTag);
-            var rendered = Mustache.render(templateTag, this.getTemplateData());
-        }
-    }
-
-    function generateTestData() {
-        let items = [];
-
-        for (let i = 0; i < 10; i++) {
-            items.push({index: getNextIndex(), text: "Item " + i});
-        }
-
-        function getNextIndex() {
-            return listItems.length;
-        }
-
-        return items;
-    }
-
-    function setupData() {
-        Array.prototype.push.apply(listItems, generateTestData());
-    }
-
-    const ClickModes = {
+    let _ClickModes = {
         CTRL_CLICK_TO_SELECT: 0,
         CLICK_TO_SELECT: 1
     };
 
-    const clickMode = ClickModes.CLICK_TO_SELECT;
+    let _clickMode;
+    let _allItemElems = [];
+    let _$allItemsElems = [];
+    let _lastClickedIndexWithoutShift = null;
+    let _newSelection = [];
+    let _selectedClass;
+    let _itemsSelector;
 
-    let allItemElems = document.querySelectorAll('.list li');
-    let $allItemsElems = $('.list li');
-    let totalItemCount = allItemElems.length;
-    let lastClickedIndexWithoutShift = null;
-    let newSelection = [];
+    function init({clickMode, containerSelector, childSelector, resetSelector, selectedClass}) {
+        _clickMode = clickMode;
+        _selectedClass = selectedClass;
+        _itemsSelector = containerSelector + " " + childSelector;
 
-    function setupBindings() {
-        let listElem = document.querySelector('.list');
-        let resetElem = document.querySelector('.reset');
+        _allItemElems = document.querySelectorAll(_itemsSelector);
+        _$allItemsElems = $(_itemsSelector); // TODO: Remove jQuery dependency?
+
+        let listElem = document.querySelector(containerSelector);
+        let resetElem = document.querySelector(resetSelector);
 
         listElem.addEventListener('selectstart', (e) => {
             // disable selecting text
@@ -61,7 +31,7 @@
 
         resetElem.addEventListener('click', clearAllSelections);
 
-        [...allItemElems].forEach(elem => elem.addEventListener('click', function(e) {
+        [..._allItemElems].forEach(elem => elem.addEventListener('click', function (e) {
             updateSelection.call(this, e, updateDOM);
         }));
     }
@@ -69,18 +39,18 @@
     function updateDOM(selection) {
         let sortedSelection = selection.concat().sort();
 
-        for (let i = 0; i < totalItemCount; i++) {
-            let item = $allItemsElems.get(i);
+        for (let i = 0; i < _allItemElems.length; i++) {
+            let item = _$allItemsElems.get(i);
 
             // TODO: This will cause reflow because we are reading and writing to the dom. Need to look into performance improvements here
             if (item) {
                 if (sortedSelection.includes($(item).index())) {
-                    if(!isItemSelected(item)) {
-                        $(item).addClass('selected');
+                    if (!isItemSelected(item)) {
+                        $(item).addClass(_selectedClass);
                     }
                 } else {
                     if (isItemSelected(item)) {
-                        $(item).removeClass('selected');
+                        $(item).removeClass(_selectedClass);
                     }
                 }
             }
@@ -91,69 +61,77 @@
         let $listItem = $(this);
 
         if (!e.shiftKey) {
-            lastClickedIndexWithoutShift = $listItem.index();
+            _lastClickedIndexWithoutShift = $listItem.index();
 
             if (isItemSelected($listItem)) {
-                if (clickMode === ClickModes.CTRL_CLICK_TO_SELECT && !e.ctrlKey) {
+                if (_clickMode === _ClickModes.CTRL_CLICK_TO_SELECT && !e.ctrlKey) {
                     // if user clicks without CTRL key, clear everything and select the one they clicked on
-                    newSelection = [];
-                    newSelection.push($listItem.index());
+                    _newSelection = [];
+                    _newSelection.push($listItem.index());
                 } else {
                     // CTRL clicking or clicking in CLICK_TO_SELECT mode will unselect the item
-                    newSelection.splice(newSelection.indexOf($listItem.index()), 1);
+                    _newSelection.splice(_newSelection.indexOf($listItem.index()), 1);
                 }
             } else {
-                if (clickMode === ClickModes.CTRL_CLICK_TO_SELECT && !e.ctrlKey) {
+                if (_clickMode === _ClickModes.CTRL_CLICK_TO_SELECT && !e.ctrlKey) {
                     // clear all selected items first if not using CTRL key
-                    newSelection = [];
+                    _newSelection = [];
                 }
 
-                newSelection.push($listItem.index());
+                _newSelection.push($listItem.index());
             }
         } else {
-            let firstSelectedItemIndex = $('.list li.selected').first().index();
-            let selectedItemIndex = $allItemsElems.index($listItem);
+            let firstSelectedItemIndex = $(_itemsSelector + "." + _selectedClass).first().index();
+            let selectedItemIndex = _$allItemsElems.index($listItem);
 
-            console.log("first selected item: ", firstSelectedItemIndex, " current selected item: ", selectedItemIndex, " last selected item without shift: ", lastClickedIndexWithoutShift);
+            console.log("first selected item: ", firstSelectedItemIndex, " current selected item: ", selectedItemIndex, " last selected item without shift: ", _lastClickedIndexWithoutShift);
 
             if (firstSelectedItemIndex === selectedItemIndex) {
                 // multiple items are selected currently and user wants to reduce range to just the selected item
-                unselectItemsWithinRange({start: selectedItemIndex + 1, end: totalItemCount, mode: 'forward'});
+                unselectItemsWithinRange({start: selectedItemIndex + 1, end: _allItemElems.length, mode: 'forward'});
             } else if (firstSelectedItemIndex < selectedItemIndex) {
-                if (selectedItemIndex > lastClickedIndexWithoutShift) {
+                if (selectedItemIndex > _lastClickedIndexWithoutShift) {
                     // user wants to add the next items up until selected item to complete a forward range
-                    selectItemsWithinRange({start: lastClickedIndexWithoutShift + 1, end: selectedItemIndex });
+                    selectItemsWithinRange({start: _lastClickedIndexWithoutShift + 1, end: selectedItemIndex});
                     // the user may be reducing the range as a result, so clear selection after the current selected item
-                    unselectItemsWithinRange({start: selectedItemIndex + 1, end: totalItemCount, mode: 'forward'});
+                    unselectItemsWithinRange({start: selectedItemIndex + 1, end: _allItemElems.length, mode: 'forward'});
                     // if a previous selection is before the last clicked index without a shift, we need to clear it
-                    unselectItemsWithinRange({start: lastClickedIndexWithoutShift - 1, end: 0, mode: 'reverse'});
+                    unselectItemsWithinRange({start: _lastClickedIndexWithoutShift - 1, end: 0, mode: 'reverse'});
                 } else {
 
                     // user is selecting from the selected item to the last clicked item without shift
-                    selectItemsWithinRange({start: selectedItemIndex, end: lastClickedIndexWithoutShift -1 });
+                    selectItemsWithinRange({start: selectedItemIndex, end: _lastClickedIndexWithoutShift - 1});
                     // the user is reducing the selection above the last clicked index without shift, so we need to clear selection before the current selected item
                     unselectItemsWithinRange({start: selectedItemIndex - 1, end: 0, mode: 'reverse'});
                     // clear items after the last clicked index without shift
-                    unselectItemsWithinRange({start: lastClickedIndexWithoutShift + 1, end: totalItemCount, mode: 'forward'});
+                    unselectItemsWithinRange({
+                        start: _lastClickedIndexWithoutShift + 1,
+                        end: _allItemElems.length,
+                        mode: 'forward'
+                    });
                 }
             } else {
                 // user is selecting range upwards, so select between the last clicked index without shift and the selected item
-                selectItemsWithinRange({start: selectedItemIndex, end: lastClickedIndexWithoutShift });
+                selectItemsWithinRange({start: selectedItemIndex, end: _lastClickedIndexWithoutShift});
                 // clear items after the last clicked index without shift
-                unselectItemsWithinRange({start: lastClickedIndexWithoutShift + 1, end: totalItemCount, mode: 'forward'});
+                unselectItemsWithinRange({
+                    start: _lastClickedIndexWithoutShift + 1,
+                    end: _allItemElems.length,
+                    mode: 'forward'
+                });
             }
         }
 
-        updateDOM(newSelection);
+        updateDOM(_newSelection);
     }
 
     function selectItemsWithinRange({start, end}) {
         if (start < 0 || end < start) return;
 
         for (let i = start; i <= end; i++) {
-            let item = $allItemsElems.get(i);
+            let item = _$allItemsElems.get(i);
             if (item && !isItemSelected(item)) {
-                newSelection.push($(item).index());
+                _newSelection.push($(item).index());
             }
         }
     }
@@ -172,26 +150,31 @@
         }
 
         function unselectItemAtIndex(index) {
-            let item = $allItemsElems.get(index);
+            let item = _$allItemsElems.get(index);
             if (item && isItemSelected(item)) {
-                newSelection.splice(newSelection.indexOf(index), 1);
+                _newSelection.splice(_newSelection.indexOf(index), 1);
             }
         }
     }
 
     function isItemSelected(item) {
-        return $(item).hasClass('selected');
+        return $(item).hasClass(_selectedClass);
     }
 
     function clearAllSelections() {
-        newSelection = [];
-        updateDOM(newSelection);
+        _newSelection = [];
+        updateDOM(_newSelection);
     }
 
-    setupBindings();
+    let SimpleListSelectionRange = {
+        init,
+        ClickModes: _ClickModes
+    };
 
-
-    init();
-
-
+    if (typeof module !== "undefined") {
+        module.exports = SimpleListSelectionRange;
+    } else {
+        window.SimpleListSelectionRange = SimpleListSelectionRange
+    }
 })();
+
